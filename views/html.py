488b88fd23a5845df0719ca3bf164767371e336a
jackdev085 <computadordb@gmail.com>
2024-12-26 15:45:33 -0300

    Revitalizando

--- views/html.py
+++ views/html.py
@@ -3,11 +3,11 @@ import os
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
-with open(f"{ROOT_PATH}\\html\\index.html", "r",encoding="utf8") as file:
+with open(f"{ROOT_PATH}/html/index.html", "r",encoding="utf8") as file:
     home_content = file.read()
 
-with open(f"{ROOT_PATH}\\html\\create.html", "r",encoding="utf8") as file:
+with open(f"{ROOT_PATH}/html/create.html", "r",encoding="utf8") as file:
     create_content = file.read()
 
-with open(f"{ROOT_PATH}\\html\\update.html", "r",encoding="utf8") as file:
+with open(f"{ROOT_PATH}/html/update.html", "r",encoding="utf8") as file:
     update_content = file.read()
